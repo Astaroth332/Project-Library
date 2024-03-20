782fca571a80504053eab0368f604add5152afaf
Astaroth332 <nomairiguro66@gmail.com>
2024-03-20 10:47:59 +0800

    Done transtition from constructor to class

--- script.js
+++ script.js
@@ -1,51 +1,122 @@
-let library = [];
+class BookLibrary {
+    constructor() {
+        this.library = [];
+    }
 
-function generateID() {
-    return Math.floor(Date.now() % (Math.random() * 1000))
+    generateID() {
+        return Math.floor(Date.now() % (Math.random() * 1000));
+    }
+
+    addBookToLibrary(name, author, pages, status) {
+        const newBook = new Book(name, author, pages, status, this.generateID());
+        this.library.push(newBook);
+    }
+
+    updateTotalBookCount() {
+        const totalBook = document.querySelector('.total-book div + span');
+        totalBook.textContent = this.library.length;
+    }
+
+    updateTotalReadAndUnreadCount() {
+        const totalRead = document.querySelector('.total-read-book div + span');
+        const totalUnread = document.querySelector('.total-unread-book div + span');
+        let readCount = 0;
+        let unreadCount = 0;
+
+        this.library.forEach(book => {
+            if (book.status === "Read") {
+                readCount++;
+            }   else if(book.status === "Not read") {
+                unreadCount++;
+            }
+        });
+
+        totalRead.textContent = readCount;
+        totalUnread.textContent = unreadCount;
+    }
+
+    displayBook() {
+            const cardContainer = document.querySelector('.card-container');
+            cardContainer.innerHTML = "";
+            let index = 0;
+        
+            for(const book of this.library) {
+                const div = document.createElement('div');
+                div.classList.add('card');
+                div.setAttribute('data-book-index', index); 
+                div.dataset.bookIndex = index++;
+                div.innerHTML = `
+                <div class="book-title">
+                            <p>Title:</p>
+                            <p>${book.name}</p>
+                        </div>
+                        <div class="book-author">
+                            <p>Author:</p>
+                            <p>${book.author}</p>
+                        </div>
+                        <div class="book-pages">
+                            <p>Pages:</p>
+                            <p>${book.pages}</p>
+                        </div>
+                        <div class="book-status">
+                            <p>Status:</p>
+                            <p>${book.status}</p>
+                        </div>
+                        <div class="button">
+                            <div class="read-button">
+                                <button>Read</button>
+                            </div>
+                            <div class="delete-button">
+                                <button id="delete";>Delete</button>
+                            </div>
+                        </div>
+                `
+                cardContainer.appendChild(div);
+            }
+
+            const deleteButtons = document.querySelectorAll('.delete-button button');
+                deleteButtons.forEach(button => {
+                    button.addEventListener('click', (e) => {
+                        const card = e.target.closest('.card');
+                        const cardIndex = parseInt(card.dataset.bookIndex);
+                        const bookID = this.library[cardIndex].id;
+                        this.library = this.library.filter(book => book.id !== bookID);
+                        this.updateTotalBookCount();
+                        this.updateTotalReadAndUnreadCount();
+                        this.displayBook();
+                    });
+                });
+
+            const readButtons = document.querySelectorAll('.read-button button');
+                readButtons.forEach(button => {
+                    button.addEventListener('click',()  => {
+                        const card = button.closest('.card');
+                        const cardIndex = parseInt(card.dataset.bookIndex);
+                        const currentBook = this.library[cardIndex];
+                        currentBook.toggleReadStatusButton();
+                        this.updateTotalReadAndUnreadCount();
+                        this.displayBook();
+                    });
+                });
+    }
+
 }
 
-function Book(name, author, pages, status) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-    this.id = generateID();
-};
- 
-function addBookToLibrary(name, author, pages, status) {
-    let newBook = new Book(name, author, pages, status);
-    library.push(newBook);
-    updateTotalBookCount();
-    updateTotalReadAndUnreadCount();
+class Book {
+    constructor(name,author,pages,status,id) {
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+        this.id = id;
+    }
+
+    toggleReadStatusButton() {
+        this.status = this.status === "Read" ? "Not read" : "Read";
+    }
 }
 
-function updateTotalBookCount() {
-    const totalBook = document.querySelector('.total-book div + span');
-    totalBook.textContent = library.length;
-}
-
-Book.prototype.toggleReadStatusButton = function(){
-    this.status = this.status === "Read" ? "Not read" : "Read";
-    updateTotalReadAndUnreadCount();
-}
-
-function updateTotalReadAndUnreadCount() {
-    const totalRead = document.querySelector('.total-read-book div + span');
-    const totalUnread = document.querySelector('.total-unread-book div + span');
-    let readCount = 0;
-    let unreadCount = 0;
-
-    library.forEach(book => {
-        if (book.status === "Read") {
-            readCount++;
-        }else if (book.status === "Not read") {
-            unreadCount++;
-        }
-    });
-
-    totalRead.textContent = readCount;
-    totalUnread.textContent = unreadCount;
-}
+let myLibrary = new BookLibrary();
 
 const btnToShowDialog = document.getElementById('show-dialog');
 const popUpDialog = document.getElementById('add-book-dialog');
@@ -64,83 +135,16 @@ popUpDialog.addEventListener('close', () => {
     inputAuthor.value = "";
     inputPages.value = "";
     inputStatus.checked = false;
-
 });
 
 confirmBtn.addEventListener('click', (e) => {
     e.preventDefault();
     let status = inputStatus.checked ? "Read" : "Not read yet";
-    if (inputTitle.value != "" && inputAuthor.value != "" && inputPages.value != "" && status != "" ) {
-        addBookToLibrary(inputTitle.value, inputAuthor.value, inputPages.value, status);
-        displayBook();
+    if (inputTitle.value != "" && inputAuthor.value != "" && inputPages.value != "") {
+        myLibrary.addBookToLibrary(inputTitle.value, inputAuthor.value, inputPages.value, status);
+        myLibrary.updateTotalBookCount();
+        myLibrary.updateTotalReadAndUnreadCount();
+        myLibrary.displayBook();
     }
     popUpDialog.close();
 });
-
-function displayBook() {
-    const cardContainer = document.querySelector('.card-container');
-    cardContainer.innerHTML = "";
-    let index = 0;
-
-    for(const book of library) {
-        const div = document.createElement('div');
-        div.classList.add('card');
-        div.setAttribute('data-book-index', index); 
-        div.dataset.bookIndex = index++;
-        div.innerHTML = `
-        <div class="book-title">
-                    <p>Title:</p>
-                    <p>${book.name}</p>
-                </div>
-                <div class="book-author">
-                    <p>Author:</p>
-                    <p>${book.author}</p>
-                </div>
-                <div class="book-pages">
-                    <p>Pages:</p>
-                    <p>${book.pages}</p>
-                </div>
-                <div class="book-status">
-                    <p>Status:</p>
-                    <p>${book.status}</p>
-                </div>
-                <div class="button">
-                    <div class="read-button">
-                        <button>Read</button>
-                    </div>
-                    <div class="delete-button">
-                        <button id="delete";>Delete</button>
-                    </div>
-                </div>
-        `
-        cardContainer.appendChild(div);
-        
-    }
-
-    const deleteButtons = document.querySelectorAll('.delete-button button');
-    deleteButtons.forEach(button => {
-        button.addEventListener('click', (e) => {
-            const card = e.target.closest('.card');
-            const cardIndex = parseInt(card.dataset.bookIndex);
-            const bookID = library[cardIndex].id;
-            library = library.filter(book => book.id !== bookID);
-            updateTotalBookCount();
-            updateTotalReadAndUnreadCount();
-            displayBook();
-        });
-    });
-
-    const readButtons = document.querySelectorAll('.read-button button');
-    readButtons.forEach(button => {
-        button.addEventListener('click',(e)  => {
-            const card = button.closest('.card');
-            const cardIndex = parseInt(card.dataset.bookIndex);
-            const currentBook = library[cardIndex];
-            currentBook.toggleReadStatusButton();
-            displayBook();
-        });
-    });
-
-}
-
-displayBook();
